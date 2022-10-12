b256aaaa77e2903ef7ec16034ae107b0d6db7091
greeshma0810 <88252520+greeshma0810@users.noreply.github.com>
2022-10-12 10:16:14 +0530

    Add files via upload

---
+++ flight-user/src/main/java/com/capg/FlightUserApplication.java
@@ -0,0 +1,23 @@
+package com.capg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.capg.entity")
+@ComponentScan(basePackages = "com.capg.controller")
+@ComponentScan(basePackages = "com.capg.service")
+@ComponentScan(basePackages = "com.capg.dto")
+@ComponentScan(basePackages = "com.capg.security")
+@EnableMongoRepositories("com.capg.repository")
+@EnableEurekaClient
+public class FlightUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightUserApplication.class, args);
+	}
+
+}
